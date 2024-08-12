9a5b422a18dcb054f2b04f4d761220761bc2577c
QJWork <qjin21@bu.edu>
2024-08-12 13:25:52 -0400

    feat remove duplicates

--- frontend/app/lib/chatpage.dart
+++ frontend/app/lib/chatpage.dart
@@ -1,8 +1,6 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ChatPage extends StatefulWidget {
