eff44db4eb7852188fb054bf80b67ae257102467
Efrain Morales <efrainjm@bu.edu>
2024-08-16 12:02:04 -0400

    Still merging

--- frontend/app/lib/chatpage.dart
+++ frontend/app/lib/chatpage.dart
@@ -1,11 +1,5 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
 import 'package:animated_text_kit/animated_text_kit.dart';
-=======
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:http/http.dart' as http;
->>>>>>> 9a5b422a18dcb054f2b04f4d761220761bc2577c
 
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
@@ -14,7 +8,6 @@ class ChatPage extends StatefulWidget {
   ChatPageState createState() => ChatPageState();
 }
 
-<<<<<<< HEAD
 class ChatPageState extends State<ChatPage> {
   final List<ChatMessage> messages = [];
   final ScrollController scrollController = ScrollController();
@@ -64,69 +57,6 @@ class ChatPageState extends State<ChatPage> {
         isAnimated: true,
         animationSpeed: speed,
       ));
-=======
-class _ChatPageState extends State<ChatPage> {
-  final List<List<_Message>> _chatSessions = [[]];
-  int _currentChatIndex = 0;
-  final TextEditingController _controller = TextEditingController();
-  final ScrollController _scrollController = ScrollController();
-
-  @override
-  void initState() {
-    super.initState();
-    _loadChatHistory();
-  }
-
-  Future<void> _loadChatHistory() async {
-    final prefs = await SharedPreferences.getInstance();
-    final chatData = prefs.getString('chat_sessions');
-    if (chatData != null) {
-      final List<dynamic> decodedData = jsonDecode(chatData);
-      setState(() {
-        _chatSessions.clear();
-        _chatSessions.addAll(decodedData.map((chat) => (chat as List<dynamic>).map((msg) => _Message.fromJson(msg)).toList()));
-      });
-    }
-  }
-
-  Future<void> _saveChatHistory() async {
-    final prefs = await SharedPreferences.getInstance();
-    final chatData = jsonEncode(_chatSessions.map((chat) => chat.map((msg) => msg.toJson()).toList()).toList());
-    prefs.setString('chat_sessions', chatData);
-  }
-
-Future<void> _handleSubmitted(String text) async {
-  if (text.isEmpty) return;
-  setState(() {
-    _chatSessions[_currentChatIndex].add(_Message(text: text, isUser: true));
-  });
-  _controller.clear();
-  _scrollToBottom();
-
-   try {
-       final response = await http.post(Uri.parse('http://localhost:5001/query'), 
-       headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-        },
-        body: jsonEncode(<String, String>{
-          'query': text,
-        }),);
-       var answer = _Message(text: "This is a simulated bot response.", isUser: false);
-        if (response.statusCode == 200) {
-          // If the server did return a 200 OK response,
-          // then parse the JSON.
-          var json = jsonDecode(response.body) as Map<String, dynamic>;
-          json['isUser'] = false;
-          answer = _Message.fromJson(json);
-          print(answer);
-        } else {
-          // If the server did not return a 200 OK response,
-          // then throw an exception.
-          throw Exception(response.body);
-        } setState(() {
-      // For now, simulate a bot response after a delay.
-      _chatSessions[_currentChatIndex].add(answer);
->>>>>>> 9a5b422a18dcb054f2b04f4d761220761bc2577c
     });
     textController.clear();
     Future.delayed(Duration(milliseconds: 300), () {
@@ -134,40 +64,6 @@ Future<void> _handleSubmitted(String text) async {
     });
     focusNode.requestFocus();
     print(text);
-    } catch (e) {
-      print('Error: $e');
-    }
-    _controller.clear();
-
-  _scrollToBottom();
-  _saveChatHistory(); // Save the chat history after receiving the bot response
-}
-
-  void _startNewChat() {
-    setState(() {
-      _chatSessions.add([]);
-      _currentChatIndex = _chatSessions.length - 1;
-    });
-    _saveChatHistory();
-    Navigator.pop(context); // Close the drawer
-  }
-
-  void _switchChat(int index) {
-    setState(() {
-      _currentChatIndex = index;
-    });
-    Navigator.pop(context); // Close the drawer
-    _scrollToBottom();
-  }
-
-  void _scrollToBottom() {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _scrollController.animateTo(
-        _scrollController.position.maxScrollExtent,
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.easeOut,
-      );
-    });
   }
 
   void scrollToBottom() {
@@ -188,44 +84,12 @@ Future<void> _handleSubmitted(String text) async {
 
     return Scaffold(
       appBar: AppBar(
-<<<<<<< HEAD
         title: const Text('Poseidon-Bot'),
         backgroundColor: Color.fromARGB(255, 173, 232, 245),
-=======
-        backgroundColor: const Color.fromARGB(255, 173, 232, 245),
-        title: const Text('Poseidon Bot', style: TextStyle(color: Colors.white)),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 129, 241, 194),
-              ),
-              child: Text(
-                'Past Chats',
-                style: TextStyle(color: Color.fromARGB(192, 115, 115, 115), fontSize: 24),
-              ),
-            ),
-            for (int i = 0; i < _chatSessions.length; i++)
-              ListTile(
-                title: Text('Chat ${i + 1}'),
-                onTap: () => _switchChat(i),
-              ),
-            ListTile(
-              leading: const Icon(Icons.add),
-              title: const Text('New Chat'),
-              onTap: _startNewChat,
-            ),
-          ],
-        ),
->>>>>>> 9a5b422a18dcb054f2b04f4d761220761bc2577c
       ),
       body: Column(
         children: <Widget>[
           Expanded(
-<<<<<<< HEAD
             child: Padding(
               padding: EdgeInsets.only(bottom: messageBarHeight),
               child: ListView.builder(
@@ -384,103 +248,8 @@ class ChatMessageWidget extends StatelessWidget {
               style: TextStyle(
                 color: message.isUser ? Colors.black87 : Colors.black87,
               ),
-=======
-            child: ListView.builder(
-              padding: const EdgeInsets.all(8.0),
-              controller: _scrollController,
-              itemCount: _chatSessions[_currentChatIndex].length,
-              itemBuilder: (context, index) {
-                return _MessageWidget(
-                  message: _chatSessions[_currentChatIndex][index],
-                );
-              },
             ),
           ),
-          const Divider(height: 1.0),
-          Container(
-            color: Colors.white,
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    decoration: const InputDecoration.collapsed(
-                      hintText: 'Type your message here...',
-                    ),
-                    onSubmitted: _handleSubmitted,
-                  ),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.send),
-                  onPressed: () => _handleSubmitted(_controller.text),
-                ),
-              ],
->>>>>>> 9a5b422a18dcb054f2b04f4d761220761bc2577c
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class _Message {
-  final String text;
-  final bool isUser;
-
-  _Message({required this.text, required this.isUser});
-
-  Map<String, dynamic> toJson() => {
-        'text': text,
-        'isUser': isUser,
-      };
-
-  factory _Message.fromJson(Map<String, dynamic> json) => _Message(
-        text: json['text'],
-        isUser: json['isUser'],
-      );
-}
-
-class _MessageWidget extends StatelessWidget {
-  final _Message message;
-
-  const _MessageWidget({required this.message});
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 10.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment:
-            message.isUser ? MainAxisAlignment.end : MainAxisAlignment.start,
-        children: <Widget>[
-          if (!message.isUser)
-            const CircleAvatar(
-              child: Text('AI'),
-              backgroundColor: Colors.grey,
-            ),
-          if (!message.isUser) const SizedBox(width: 8.0),
-          Container(
-            padding: const EdgeInsets.all(10.0),
-            constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.75),
-            decoration: BoxDecoration(
-              color: message.isUser ? Colors.blueAccent : Colors.grey[300],
-              borderRadius: BorderRadius.circular(12.0),
-            ),
-            child: Text(
-              message.text,
-              style: TextStyle(
-                color: message.isUser ? Colors.white : Colors.black87,
-              ),
-            ),
-          ),
-          if (message.isUser) const SizedBox(width: 8.0),
-          if (message.isUser)
-            const CircleAvatar(
-              child: Text('U'),
-              backgroundColor: Colors.blueAccent,
-            ),
         ],
       ),
     );
